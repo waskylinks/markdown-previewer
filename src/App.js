e7f1fb7dc08567737ce94ec4a858b2c340570d67
waskylinks <likitajoel@gmail.com>
2025-07-15 11:02:56 +0100

    markdown previewer js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
+import React, { useState }from 'react';
 import './App.css';
+import {marked} from 'marked';
+
+marked.setOptions({ breaks: true });
+
+const defaultMarkdown = `# Welcome tp my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Here's some code, \`<div></div>\`,
+between 2 backticks.
+
+\`\`\`
+// this is a multi line code:
+function anotherExample(firstLine,
+lastLine) {
+  if (firstLine === '\`\`\`' && lastLine === '\`\`\`') {
+  return multiLineCode;
+  }
+};
+\`\`\`
+
+You can also make text **bold**...
+whoa!
+Or _italics_.
+Or ... wait for it... **_bold!_**
+And feel free to go crazy ~~crossing
+stuff out~~.
+
+There's also 
+[links](https://www.freecodecamp.org)
+, and
+> Block Quotes!
+
+And if you want to get really crazy,
+even tables:
+
+Wild Header   | Crazy Header  | Another Header
+------------  | ------------  | -------------
+content can   | be here, and  | here
+
+- And of course there are lists.
+  - Some are bulleted
+    - With different indentation levels.
+      - That looks like this.
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freecodecampLogo](https://cdn.freecodecamp.org/testables-projects-free/images/fcc_secoundary.svg)
+  
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+  const [editorVisible, setEditorVisible] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+  <div
+  className={`app ${darkMode ? 'dark' : 'light'}`}
+  >
+    <div className='theme-toggle'>
+      <label>
+        <input
+        type='checkbox'
+        checked={darkMode}
+        onChange={ () => setDarkMode(!darkMode)}
+        />
+        {darkMode ? ' Dark Mode' : ' Light Mode'}
+      </label>
+    </div>
+    
+    <div
+    className='pane editor-pane'
+    style={{ display: editorVisible ? 'flex' : 'none'}}
+    >
+      <div
+      className='toolbar'>
+        <span
+        className='title'>
+          Editor
+        </span>
+        <button
+        className='close-btn'
+        onClick={() => setEditorVisible(false)}
+        >
+          X
+        </button>
+      </div>
+      <textarea
+      id='editor'
+      value={markdown}
+      onChange={(e) => setMarkdown(e.target.value)}
+      />
+    </div>
+
+    <div
+    className='pane-preview-pane'
+    style={{ display: editorVisible ? 'none' : 'flex'}}
+    >
+
+      <div className='toolbar'>
+        <span className='title'>Previewer</span>
+        <button
+        className='close-btn'
+        onClick={() => setEditorVisible(true)}>
+          X
+        </button>
+      </div>
+      <div
+      id='preview'
+      dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }} 
+      />
+
+    </div>
+
+  </div>
+)}
+
+export default App
